0a332dd15c6ad5d47d407843aa30c4e1e56ccfb4
Niklas <68104861+nilmarinen@users.noreply.github.com>
2026-02-17 16:44:35 +0200

    Add sb_medium function for recipe generation

---
+++ sb-medium_recipe.py
@@ -0,0 +1,19 @@
+def sb_medium(liters):
+    p = liters * 30
+    y = liters * 20
+    m = liters * 10
+    n = liters * 0.700
+
+    print("Recipe for",liters, "L of SB-medium\n"
+    "\n"
+    "Peptone:",p,"g\n"
+    "Yeast extract:",y,"g\n"
+    "MOPS:",m,"g\n"
+    "NaOH:",n,"g\n"
+    "Fill to",0.9*liters*1000,"mL mQ-H2O.\n"
+    "Adjust pH to 7.0 with NaOH or HCl.\n"
+    "Fill to",liters,"L.\n"
+    "Sterilize bu autoclaving.")
+
+
+sb_medium(4)
